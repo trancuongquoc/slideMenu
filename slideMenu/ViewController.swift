967abd79ec92d3cf90666341e0f25bc449a762c1
quoccuong <tran.cuongquoc@gmail.com>
2018-06-02 16:02:23 +0700

    no message

--- slideMenu/ViewController.swift
+++ slideMenu/ViewController.swift
@@ -10,16 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slideViewLeading: NSLayoutConstraint!
+    @IBOutlet weak var slideView: UIView!
+    @IBOutlet weak var hiddenButton: UIButton!
+    
+    var slideMenuIsVisible = false {
+        didSet {
+            UIView.animate(withDuration: 0.2, delay: 0.0, options: .curveEaseIn, animations: {
+                if self.slideMenuIsVisible {
+                    self.slideViewLeading.constant = 0
+                    self.hiddenButton.alpha = 1 
+                } else {
+                    self.hiddenButton.alpha = 0
+                    self.slideViewLeading.constant = -(2 * self.slideView.frame.width)
+                }
+                self.view.layoutIfNeeded()
+            }) { (animationCompletet) in
+                
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        slideMenuIsVisible = false
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func hideSlideMenu(_ sender: UIButton) {
+        slideMenuIsVisible = !slideMenuIsVisible
+    }
+    
+    @IBAction func hamburgerButtonTapped(_ sender: UIBarButtonItem) {
+        slideMenuIsVisible = !slideMenuIsVisible
+    }
+    
 }
 
